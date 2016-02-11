2b83797918c97866be2e75bacd8959548e19c40c
Hung <Hung>
2016-02-11 12:56:51 -0800

    initial commit

---
+++ src/com/example/hwapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.hwapp;
+
+/**
+ * Created by Hung on 2/11/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
